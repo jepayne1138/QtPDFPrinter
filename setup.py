44c2c721324b5b46d232155fbb1c31a8c03a3092
james.payne <jepayne1138@gmail.com>
2016-02-05 16:30:46 -0600

    Add setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='QtPDFPrinter',
+    packages=['qtpdfprinter'],
+    version='1.0a1',
+    description='Creates PDFs from HTML or Jinja2 Templates using PySide',
+    author='James Payne',
+    author_email='jepayne1138@gmail.com',
+    url='https://github.com/jepayne1138/QtPDFPrinter',
+    license='MIT',
+    download_url='https://github.com/jepayne1138/QtPDFPrinter/tarball/1.0a1',
+    keywords='html pyside html convert pdf jinja',
+    install_requires=['PySide', 'Jinja2'],
+    classifiers=[],
+)
